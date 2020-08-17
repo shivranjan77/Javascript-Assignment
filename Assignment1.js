2fa590976c0b7c8714b496f35693d15b196ec1c2
Shivranjan Kumar <shivranjan77@gmail.com>
2020-08-17 12:18:19 +0530

    completed

--- Assignment1.js
+++ Assignment1.js
@@ -13,13 +13,13 @@ Don't forget to return the result.
 If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.*/
 
-/*var a=5;
+var a=5;
 
 var sec=60;
 function converter(a){
     return console.log(a*sec);
 }
-converter(a);*/
+converter(a);
 
 /*Q2. Divides Evenly
 Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
@@ -97,19 +97,128 @@ Notes
 The input will always be in lowercase.*/ 
 
 
-var string;
-function replaceVowel(string){
-    //const len=string.length;
-        string.replace('a','1');
-        string.replace('e','2');
-        string.replace('i','3');
-        string.replace('o','4');
-        string.replace('u','5');
-        return string;
-    
+/*function replaceVowel(string) {
+  for (var place = 0; place < string.length; place++) {
+    if (string.charAt(place) == "a") {
+      string = string.replace(string.charAt(place), 1);
+    }
+    if (string.charAt(place) == "e") {
+      string = string.replace(string.charAt(place), 2);
+    }
+    if (string.charAt(place) == "i") {
+      string = string.replace(string.charAt(place), 3);
+    }
+    if (string.charAt(place) == "o") {
+      string = string.replace(string.charAt(place), 4);
+    }
+    if (string.charAt(place) == "u") {
+      string = string.replace(string.charAt(place), 5);
+    }
+  }
+  return string;
 }
-var newString=replaceVowel("karachi");
-console.log(newString);
+console.log(replaceVowel("karachi"));
+console.log(replaceVowel("chembur"));
+console.log(replaceVowel("khandbari"));*/
+               
+
+                                                 // MEDIUM
+/*console.log("-----question1-----");
+function specialReverse(string, ch) {
+  let arr = string.split(" ");
+  let len = arr.length;
+  for (i = 0; i < len; i++) {
+    if (arr[i].substr(0, 1) == ch) {
+      let newarr = arr[i].split("");
+      arr[i] = newarr.reverse().join("");
+    }
+  }
+  return arr.join(" ");
+}
+console.log(specialReverse("word searches are super fun", "s"));
+console.log(specialReverse("first man to walk on the moon", "m"));
+console.log(specialReverse("peter piper picked pickled peppers", "p"));
+console.log("------question2------");
+function testJackpot(arr) {
+  if (
+    arr[0] == arr[1] &&
+    arr[1] == arr[2] &&
+    arr[2] == arr[3] &&
+    arr[3] == arr[0]
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(testJackpot(["@", "@", "@", "@"]));
+console.log(testJackpot(["abc", "abc", "abc", "abc"]));
+console.log(testJackpot(["SS", "SS", "SS", "SS"]));
+console.log(testJackpot(["&&", "&", "&&&", "&&&&"]));
+console.log(testJackpot(["SS", "SS", "SS", "Ss"]));
+
+console.log("-----question3-----");
+function removeDups(array) {
+  return array.filter((a, b) => array.indexOf(a) == b);
+}
+console.log(removeDups([1, 0, 1, 0]));
+console.log(removeDups(["The", "big", "cat"]));
+console.log(removeDups(["John", "Taylor", "John"]));*/
+                                               
+                                               //HARD
+
+/*console.log("-----question1");
+
+function realType(value) {
+  let arr = Object.prototype.toString.call(value);
+  let type = arr.split(" ");
+  let typeStr = "";
+
+  for (i = 0; i < type[1].length; i++) {
+    if (type[1].charAt(i) !== "]") typeStr += type[1].charAt(i);
+  }
+
+  return typeStr.toLocaleLowerCase();
+}
+
+console.log(realType(1));
+console.log(realType("a"));
+console.log(realType(true));
+console.log(realType([]));
+console.log(realType(null));
+
+console.log("------question2------");
+
+function numInStr(string) {
+  let finalArray = [];
+  for (i = 0; i < string.length; i++) {
+    arr = string[i].split("");
+
+    for (j = 0; j < arr.length; j++) {
+      if (
+        arr[j] == "1" ||
+        arr[j] == "2" ||
+        arr[j] == "3" ||
+        arr[j] == "4" ||
+        arr[j] == "5" ||
+        arr[j] == "6" ||
+        arr[j] == "7" ||
+        arr[j] == "8" ||
+        arr[j] == "9" ||
+        arr[j] == "0"
+      ) {
+        finalArray.push(arr.join(""));
+        break;
+      }
+    }
+  }
+  return finalArray;
+}
+
+console.log(numInStr(["1a", "a", "2b", "b"]));
+console.log(numInStr(["abc", "abc10"]));
+console.log(numInStr(["abc", "ab10c", "ab10c", "bcd"]));
+console.log(numInStr(["this is a test", "test1"]));*/
 
 
 
